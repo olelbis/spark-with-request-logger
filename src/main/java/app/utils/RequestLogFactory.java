4a267f8fd35f9098e7a2895315b94ee6500abce0
Marco Gianini <marco.gianini.it@gmail.com>
2021-07-14 23:45:54 +0200

    Some improvement

--- src/main/java/app/utils/RequestLogFactory.java
+++ src/main/java/app/utils/RequestLogFactory.java
@@ -2,6 +2,8 @@ package app.utils;
 
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.CustomRequestLog;
+import org.eclipse.jetty.server.RequestLogWriter;
+
 
 
 public class RequestLogFactory {
@@ -12,9 +14,15 @@ public class RequestLogFactory {
     public RequestLogFactory(Logger logger) {
         this.logger = logger;
     }
-
+    RequestLogWriter rlw = new RequestLogWriter();
+    
     CustomRequestLog create() {
-        return new CustomRequestLog(null) {
+    	rlw.setTimeZone("GMT");
+    	rlw.setFilename("http-yyyy_MM_dd.log");
+    	rlw.setFilenameDateFormat("yyyy_MM_dd");
+    	rlw.setAppend(false);
+    	//System.out.println(rlw.getFilenameDateFormat());
+        return new CustomRequestLog(rlw,CustomRequestLog.EXTENDED_NCSA_FORMAT) {
         };
     }
 }
