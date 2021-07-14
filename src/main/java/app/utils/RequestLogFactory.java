a592d7f046bae24f80094fd586de2953219bc35c
Marco Gianini <marco.gianini.it@gmail.com>
2021-07-14 15:28:19 +0200

    Code review

--- src/main/java/app/utils/RequestLogFactory.java
+++ src/main/java/app/utils/RequestLogFactory.java
@@ -3,11 +3,11 @@ package app.utils;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jetty.server.CustomRequestLog;
 
-import java.io.IOException;
 
 public class RequestLogFactory {
 
-    private Logger logger;
+    @SuppressWarnings("unused")
+	private Logger logger;
 
     public RequestLogFactory(Logger logger) {
         this.logger = logger;
@@ -15,15 +15,6 @@ public class RequestLogFactory {
 
     CustomRequestLog create() {
         return new CustomRequestLog(null) {
-            @SuppressWarnings("unused")
-			protected boolean isEnabled() {
-                return true;
-            }
-
-            @SuppressWarnings("unused")
-			public void write(String s) throws IOException {
-                logger.info(s);
-            }
         };
     }
 }
